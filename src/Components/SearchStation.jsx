bc8821c2615c9a776753fe81d120651ca270ef34
dd3dd <jacky81810@gmail.com>
2023-10-28 23:35:40 +0800

    dsds

--- src/Components/SearchStation.jsx
+++ src/Components/SearchStation.jsx
@@ -3,11 +3,17 @@ import { useContext } from 'react'
 import UserContext from '../Context/UserContext'
 export default function SearchStation() {
     const { searchText, setSearchText } = useContext(UserContext);
+
+
     const handleInput = (e) => {
         //console.log(e.target.value)
         setSearchText(e.target.value)
     }
     if (searchText !== null) {
+        // const suggest = data.map((e) => {
+        //     return e.sna.slice(11);
+        // })
+
         return (
             <input value={searchText} onChange={handleInput} className='SearchStation' placeholder='搜尋站點' type="text" />
         )
